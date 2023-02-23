ee5a4fa4d967fe90a913ae20d3aecdee91171b29
EzequielK-source <programacionezequielk@gmail.com>
2023-02-22 21:59:37 -0300

    The variable queue_length was added to record the number of users in the queue

--- src/events/join_queue.py
+++ src/events/join_queue.py
@@ -1,9 +1,13 @@
 from flask_socketio import emit
 
+queue_length = 0
+
 
 def join_queue():
     print('users join in queue')
+    global queue
+    queue += 1
     emit('queue_status', {
-        'queue_length': 100,
+        'queue_length': queue_length,
         'actual_ticket': 99
     })
