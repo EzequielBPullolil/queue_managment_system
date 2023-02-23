e59b37728c6c95ecbcfb31eddbda045544de04de
EzequielK-source <programacionezequielk@gmail.com>
2023-02-22 21:56:56 -0300

    Remove join_queue test from test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -15,21 +15,3 @@ def test_ping_pong(sio):
     assert len(received) > 0
     assert received[0]['name'] == 'pong'
     sio.disconnect()
-
-
-def test_join_queue(app):
-    flask_client = app.test_client()
-    client1 = socketio.test_client(
-        app=app,
-        flask_test_client=flask_client
-    )
-    client1.connect()
-    assert client1.is_connected() == True
-
-    client1.emit('join_queue')
-
-    received = client1.get_received()
-    queue_status = received[0]
-    assert queue_status['name'] == 'queue_status'
-    assert queue_status['args'][0] == {
-        'queue_length': 100, 'actual_ticket': 99}
