0d750744f08e270894bf4ba4ceb128579040ee5c
EzequielK-source <programacionezequielk@gmail.com>
2023-02-22 22:30:56 -0300

    /

--- src/events/join_queue.py
+++ src/events/join_queue.py
@@ -4,6 +4,10 @@ queue_length = 0
 
 
 def join_queue():
+    '''
+        Increment queue_length and instance User object
+        for persist user_ticket
+    '''
     print('users join in queue')
     global queue_length
     queue_length += 1
