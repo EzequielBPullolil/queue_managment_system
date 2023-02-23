c8421ec69241465586db0807bf63bb9298e8081e
EzequielK-source <programacionezequielk@gmail.com>
2023-02-22 22:28:49 -0300

    Now join_queue instance User -Instance User class and parse queue_length

--- src/events/join_queue.py
+++ src/events/join_queue.py
@@ -1,13 +1,14 @@
 from flask_socketio import emit
-
+from src.entities.user import User
 queue_length = 0
 
 
 def join_queue():
     print('users join in queue')
-    global queue
-    queue += 1
+    global queue_length
+    queue_length += 1
+    user = User(queue_length)
     emit('queue_status', {
         'queue_length': queue_length,
-        'actual_ticket': 99
+        'actual_ticket': user.actual_ticket
     })
