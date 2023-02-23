07372cdbf8867a3a43c1361b9258e6d1a82131f5
EzequielK-source <programacionezequielk@gmail.com>
2023-02-22 21:21:34 -0300

    Add test for join_queue event

--- test/test_app.py
+++ test/test_app.py
@@ -1,17 +1,35 @@
 from src.app import socketio
 
 
-def test_connection(app):
+def test_ping_pong(sio):
+    '''
+        sio test client connect app and 
+        try to emit PING and assert if 
+        they received PONG event
+    '''
+    sio.connect()
+    assert sio.is_connected() == True
+
+    sio.emit('ping')
+    received = sio.get_received()
+    assert len(received) > 0
+    assert received[0]['name'] == 'pong'
+    sio.disconnect()
+
+
+def test_join_queue(app):
     flask_client = app.test_client()
-    client_one = socketio.test_client(
+    client1 = socketio.test_client(
         app=app,
         flask_test_client=flask_client
     )
+    client1.connect()
+    assert client1.is_connected() == True
 
-    client_one.connect()
-    assert client_one.is_connected() == True
+    client1.emit('join_queue')
 
-    client_one.emit('ping')
-    received = client_one.get_received()
-    print(received)
-    assert received[0]['name'] == 'pong'
+    received = client1.get_received()
+    queue_status = received[0]
+    assert queue_status['name'] == 'queue_status'
+    assert queue_status['args'][0] == {
+        'queue_length': 100, 'actual_ticket': 99}
