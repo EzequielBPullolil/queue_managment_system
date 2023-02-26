436480f8373687ad5a14c20e9acc58da2b2a88ca
EzequielK-source <programacionezequielk@gmail.com>
2023-02-26 17:13:31 -0300

    Add two new Fixtures for conftest and call sync_db -Create user_suject_id fixture for reference an persisted user -Create queue_suject_id fixture for reference an persisted queue

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,10 @@
 import pytest
 from src.app import create_app, socketio
+from src.db import Session
+from src.db.models import Queue, User
+from uuid_extensions import uuid7
+
+import src.db.sync_db
 
 
 @pytest.fixture()
@@ -24,3 +29,27 @@ def sio(app):
         flask_test_client=flask_client
     )
     return sio
+
+
+@pytest.fixture()
+def queue_suject_id():
+    session = Session()
+    id = uuid7()
+    queue = Queue(id=id, name='test_suject_queue')
+    session.add(queue)
+    session.commit()
+    id = queue.id
+    session.close()
+    return id
+
+
+@pytest.fixture()
+def user_suject_id():
+    session = Session()
+    user = User(name='user_suject', email='test@test.com')
+    session.add(user)
+    session.commit()
+    id = user.getId()
+    session.close()
+
+    return id
