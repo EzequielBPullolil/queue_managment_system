50cec97858063ba524de44e53564c5a28d32cd7d
EzequielK-source <programacionezequielk@gmail.com>
2023-02-26 18:49:16 -0300

    Add queue_length column for Queue -Add two methods:     _getLength     _incrementLength

--- src/domain/queue/model.py
+++ src/domain/queue/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from src.db import Base
 
 
@@ -7,10 +7,17 @@ class Queue(Base):
 
     id = Column(String(36), primary_key=True)
     name = Column(String(50), nullable=False)
+    queue_length = Column(Integer, default=0)
+
+    def getLength(self) -> int:
+        return self.queue_length
+
+    def incrementLength(self):
+        self.queue_length += 1
 
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
     def __repr__(self):
-        return f"Queue(id={self.id!r}, name={self.name!r})"
+        return f"Queue(id={self.id!r}, name={self.name!r}, queue_length={self.queue_length!r})"
