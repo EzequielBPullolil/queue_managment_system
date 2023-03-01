6ccdafa80a8100798a0aeda71d7ceccc6e238712
EzequielK-source <programacionezequielk@gmail.com>
2023-03-01 13:05:28 -0300

    Add test cases for leave_queue test -Added the test case for emit leave queue without being queued -Add assert for decrement_queue_length name event

--- test/domain/queue_user/events/test_leave_queue.py
+++ test/domain/queue_user/events/test_leave_queue.py
@@ -1,5 +1,9 @@
+from pytest import raises
+from src.domain.queue_user.exceptions import NonQueuedUser
+
+
 class TestLeaveQueue:
-    def test_leave_queue_emit_decrement_queue_length(self, sio, queue_user):
+    def test_event_emit_decrement_queue_length(self, sio, queue_user):
         '''
             Verify if leave queue decrement queue_length 
         '''
@@ -13,5 +17,22 @@ class TestLeaveQueue:
 
         assert len(received) > 0
 
-        assert received[0]['name'] == 'decrement_queue'
+        assert received[0]['name'] == 'decrement_queue_length'
         assert received[0]['args'][0] == queue_user['queue_id']
+
+    def test_call_event_wiouth_begin_queued_throws_an_error(self, sio, user_suject_id, queue_suject_id):
+        '''
+            Verify if emits leave_queue wiouth stay queued 
+            raise error 
+        '''
+        sio.connect()
+        assert sio.is_connected() == True
+
+        with raises(NonQueuedUser) as exception:
+            sio.emit('leave_queue', {
+                "user_id": user_suject_id,
+                "queue_id": queue_suject_id
+            })
+            assert exception.type == NonQueuedUser
+
+        received = sio.get_received()
