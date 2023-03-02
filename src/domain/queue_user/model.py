0a93b9488f2a2aea93c05117f0345c5bedc7be6e
EzequielK-source <programacionezequielk@gmail.com>
2023-03-02 18:50:25 -0300

    Modify models -Add id argument to User init -remove nullable argument to actual_ticket column in queue_user table

--- src/domain/queue_user/model.py
+++ src/domain/queue_user/model.py
@@ -17,10 +17,13 @@ class Queue_User(Base):
     )
 
     user_ticket = Column(
-        Integer,
-        nullable=True
+        Integer
     )
 
-    def __init__(self, queue_id, user_id):
+    def __init__(self, queue_id, user_id, user_ticket):
         self.queue_id = queue_id
         self.user_id = user_id
+        self.user_ticket = user_ticket
+
+    def __str__(self) -> str:
+        return f"queued_id={self.queue_id}, user_id={self.user_id}, user_ticket={self.user_ticket}"
--- src/domain/user/model.py
+++ src/domain/user/model.py
@@ -1,17 +1,19 @@
 from sqlalchemy import Column, String, Integer
 from src.db import Base
+from uuid_extensions import uuid7
 
 
 class User(Base):
     __tablename__ = 'Users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String(36), primary_key=True)
     name = Column(String(50), nullable=False)
     email = Column(String(100), nullable=False)
 
-    def __init__(self, name, email):
+    def __init__(self, name, email, id):
         self.name = name
         self.email = email
+        self.id = id
 
     def getId(self) -> int:
         return self.id
