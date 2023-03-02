43e0b479eac3939852774a8a41bfa1c412112415
EzequielK-source <programacionezequielk@gmail.com>
2023-03-02 20:21:28 -0300

    Add assertions to /queue_user/<queue_id> get request test

--- tests/domain/queue_user/test_route.py
+++ tests/domain/queue_user/test_route.py
@@ -2,10 +2,20 @@ from flask import json
 
 
 class TestQueueUserRoute:
-    def test_get_queued_users(self, client, queue_suject_id):
+    def test_get_queued_users(self, client, queue_user):
         '''
             Query queued user in queued with queue_id
         '''
-        response = client.get(f'/queue_user/{queue_suject_id}')
+        response = client.get(f'/queue_user/{queue_user["queue_id"]}')
 
         assert response.status_code == 200
+        data = json.loads(response.get_data())
+
+        status = data['status']
+        users = data['users']
+        assert status == 'queued_users'
+        assert type(users) == list
+
+        assert len(users) > 0
+
+        assert users[0]['user_id'] == queue_user['user_id']
